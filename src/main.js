9a5dc0cc3f1a7c51790e567bdd52c812898be126
Alec Perkey <alecperkey@gmail.com>
2020-03-03 15:45:38 -0500

    initialize ApolloClient with demo graphql link, use localStorage for auth token

--- src/main.js
+++ src/main.js
@@ -3,9 +3,32 @@ import App from "./App.vue";
 import router from "./router";
 import AuthPlugin from "./plugins/auth";
 
-Vue.use(AuthPlugin);
+import ApolloClient from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
+Vue.use(AuthPlugin);
 Vue.config.productionTip = false;
+const getHeaders = () => {
+  const headers = {};
+  const token = window.localStorage.getItem("apollo-token");
+  if (token) {
+    headers.authorization = `Bearer ${token}`;
+  }
+  return headers;
+};
+// Create an http link:
+const link = new HttpLink({
+  uri: "https://hasura.io/learn/graphql",
+  fetch,
+  headers: getHeaders()
+});
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache({
+    addTypename: true
+  })
+});
 
 new Vue({
   router,
